0f91bec6c243780866aad1f785d7eea354c67004
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-06-07 18:28:29 +0000

    Fix indentation errors with modules/navlinks.js file

--- modules/navlinks.js
+++ modules/navlinks.js
@@ -1,21 +1,21 @@
 const navlinks = () => {
-const Links = document.querySelectorAll('.links');
-Links.forEach(link => {
-  link.addEventListener('click', () => {
-    const sections = document.querySelectorAll('section');
-    sections.forEach((sec) => {
-      sec.classList.add('hide');
-      if (sec.classList.contains('showElement')) {
-        sec.classList.remove('showElement')
-      }
-    })
-    const section = document.querySelector(`section.${link.classList[1]}`);
-    section.classList.add('showElement')
-    if (section.classList.contains('list-section')) {
+  const Links = document.querySelectorAll('.links');
+  Links.forEach(link =>  {
+    link.addEventListener('click', () => {
+      const sections = document.querySelectorAll('section');
+      sections.forEach((sec) => {
+        sec.classList.add('hide');
+        if (sec.classList.contains('showElement')) {
+          sec.classList.remove('showElement');
+        }
+    });
+      const section = document.querySelector(`section.${link.classList[1]}`);
+      section.classList.add('showElement');
+      if (section.classList.contains('list-section')) {
         window.location.reload();
-    }
-  })
-})
+      }
+    });
+  });
 };
 
 export default navlinks;
