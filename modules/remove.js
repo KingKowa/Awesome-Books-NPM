3560a98c43d68a5c018841d9f1649d54a752770f
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-06-07 18:15:43 +0000

    Fix indentation errors with modules/remove.js file

--- modules/remove.js
+++ modules/remove.js
@@ -1,23 +1,14 @@
-
-
-//function removeme(id) {
-    //Book.removeBook(Number(id));
-    //books = Book.getBooks();
-    //localStorage.setItem('books', JSON.stringify(books));
-    //Mybooks();
-  //}
-
-  const removeme = (Book, mybooks) => {
-    const allBooks = document.querySelectorAll('.remove-btn');
-    allBooks.forEach((oneBook) => {
-      oneBook.addEventListener('click', (e) => {
-        Book.removeBook(Number(e.target.id));
-        const books = Book.getBooks();
-        localStorage.setItem('books', JSON.stringify(books));
-        mybooks(books);
-        window.location.reload();
-      });
+const removeme = (Book, mybooks) => {
+  const allBooks = document.querySelectorAll('.remove-btn');
+  allBooks.forEach((oneBook) => {
+    oneBook.addEventListener('click', (e) => {
+      Book.removeBook(Number(e.target.id));
+      const books = Book.getBooks();
+      localStorage.setItem('books', JSON.stringify(books));
+      mybooks(books);
+      window.location.reload();
     });
-  };
-  
-  export default removeme;
+  });
+};
+
+export default removeme;
